6817381f39bf7ad0392c8fca215dbfb4411b1a76
Walyttt <976762403@qq.com>
2021-05-18 11:33:53 +0800

    Daily

---
+++ InnoProjectSystem/src/models/Project.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoProjectSystem.src.models
+{
+    class Project
+    {
+		private string pNo;
+		private string pName;
+		private string fName;
+		private string ptName;
+		private string subName;
+		private string colName;
+		private int pGroup;
+
+        public Project(string pNo = null, string pName = null, string fName = null, string ptName = null, string subName = null, string colName = null, int pGroup = 0)
+        {
+            this.PNo = pNo;
+            this.PName = pName;
+            this.FName = fName;
+            this.PtName = ptName;
+            this.SubName = subName;
+            this.ColName = colName;
+            this.PGroup = pGroup;
+        }
+
+        public string PNo { get => pNo; set => pNo = value; }
+        public string PName { get => pName; set => pName = value; }
+        public string FName { get => fName; set => fName = value; }
+        public string PtName { get => ptName; set => ptName = value; }
+        public string SubName { get => subName; set => subName = value; }
+        public string ColName { get => colName; set => colName = value; }
+        public int PGroup { get => pGroup; set => pGroup = value; }
+    }
+}
