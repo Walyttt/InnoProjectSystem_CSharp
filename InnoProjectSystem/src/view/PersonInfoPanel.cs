1e055572d329a6b8c9962cb3ba8445b3577e04c9
Walyttt <976762403@qq.com>
2021-05-22 22:13:10 +0800

    daily

--- InnoProjectSystem/src/view/PersonInfoPanel.cs
+++ InnoProjectSystem/src/view/PersonInfoPanel.cs
@@ -14,14 +14,23 @@ namespace InnoProjectSystem.src.view
     public partial class PersonInfoPanel : UserControl
     {
         public User user;
-        public PersonInfoPanel(User user)
+        public PersonInfoPanel(User u)
         {
             InitializeComponent();
-            this.user = user;
+
+            this.user = u;
+            IdTxt.Text = this.user.Id;
+            NameTxt.Text = this.user.Name;
         }
 
         private void AckBtn_Click(object sender, EventArgs e)
         {
+            if (!NewpwdTxt.Text.Equals(RNewpwdTxt.Text))
+            {
+                MessageBox.Show("新密码不一致");
+                return;
+            }
+
 
         }
     }
