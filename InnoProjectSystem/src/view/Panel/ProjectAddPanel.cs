3f9f787a41dd5289a8040ad4f780f30365396900
Walyttt <976762403@qq.com>
2021-05-26 20:10:06 +0800

    daily

--- InnoProjectSystem/src/view/Panel/ProjectAddPanel.cs
+++ InnoProjectSystem/src/view/Panel/ProjectAddPanel.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using InnoProjectSystem.src.Util;
 using InnoProjectSystem.src.models;
+using System.Text.RegularExpressions;
 
 namespace InnoProjectSystem.src.view.Panel
 {
@@ -41,9 +42,63 @@ namespace InnoProjectSystem.src.view.Panel
         //添加按钮事件
         private void AckBtn_Click(object sender, EventArgs e)
         {
+            //首先判断信息输入完整
+            if (this.IdTxt.Text == String.Empty ||
+                this.NameTxt.Text == String.Empty ||
+                this.GroupTxt.Text == String.Empty ||
+                CollegeCBox.SelectedItem == null ||
+                FacultyCBox.SelectedItem == null ||
+                ProjectTypeCBox.SelectedItem == null ||
+                SubjectCBox.SelectedItem == null)
+            {
+                MessageBox.Show("请完整填写信息");
+                return;
+            }
+
+            //判断项目编号合法性
+            if(this.IdTxt.Text.Length != 17)
+            {
+                MessageBox.Show("请填写17位项目编号");
+                return;
+            }
+
+            //判断项目批次为正整数
+            if (this.IsPNumeric(this.GroupTxt.Text))
+            {
+                MessageBox.Show("请输入正确的项目批次");
+                return;
+            }
+
+            //检测项目编号是否存在
+            object flag = null;
+            SqlConnection sqlConnection = DbUtil.getConnection();
+            string cmdTxt = "Select PName From Project Where PNo=@PNo";
+            SqlCommand cmd = new SqlCommand(cmdTxt, sqlConnection);
+            cmd.Parameters.AddWithValue("@PNo", IdTxt.Text);
+            try
+            {
+                sqlConnection.Open();
+                flag = cmd.ExecuteScalar();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                    sqlConnection.Close();
+            }
+            if (flag != null)
+            {
+                MessageBox.Show("编号已被使用");
+                return;
+            }
+
+            //开始添加
 
         }
-
 
         //填充院系单位下拉框
         private void FillCollegeCBox()
@@ -113,6 +168,12 @@ namespace InnoProjectSystem.src.view.Panel
             ProjectTypeCBox.SelectedItem = null;
             SubjectCBox.SelectedItem = null;
             return;
+        }
+
+        //判断字符串为正整数
+        private bool IsPNumeric(string value)
+        {
+            return Regex.IsMatch(value, @"^[1-9]\d*$");
         }
     }
 }
